fc1ee8fbd485dd5c61b18662cd9e372d6b22b137
Janhavi Choudhari <janhavi.choudhari@oracle.com>
2024-09-07 01:03:37 +0530

    TODO authenticate with API key

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask
 from routes import main_routes  # Import the routes module
 
 
--- backend/routes.py
+++ backend/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, jsonify
 import vertexai
-# from vertexai.preview.vision_models import Image, ImageGenerationModel
-# import google.generativeai as genai
+from vertexai.preview.vision_models import Image, ImageGenerationModel
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+import random
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,31 +14,47 @@ main_routes = Blueprint('main', __name__)
 
 @main_routes.route("/imagen", methods=["POST"])
 def imagen():
-    # gcp_project_id = '' # @param {type: "string"}   
-    # auth.authenticate_user(project_id=gcp_project_id)
-
-    # vertexai.init(project=gcp_project_id)
+    gcp_project_id = os.getenv('PROJECT_ID') # @param {type: "string"}   
+    vertexai.init(project=gcp_project_id)
+    
     #Access your Gemini API key
 
     gemini_api_secret_name = os.getenv('API_KEY')  # @param {type: "string"}
-    data = {
-        "api_key":gemini_api_secret_name
-    }
 
-    return jsonify(data)
+    try:
+        GOOGLE_API_KEY=gemini_api_secret_name
+        genai.configure(api_key=GOOGLE_API_KEY)
+    
+    except Exception as e:
+        # unknown error
+        print(f"There was an unknown error. Ensure you have a secret {gemini_api_secret_name} stored and it's a valid key from https://makersuite.google.com/app/apikey")
+        raise e
 
-    # try:
-    # GOOGLE_API_KEY=userdata.get(gemini_api_secret_name)
-    # genai.configure(api_key=GOOGLE_API_KEY)
-    # except userdata.SecretNotFoundError as e:
-    # print(f'Secret not found\n\nThis expects you to create a secret named {gemini_api_secret_name} in Colab\n\nVisit https://makersuite.google.com/app/apikey to create an API key\n\nStore that in the secrets section on the left side of the notebook (key icon)\n\nName the secret {gemini_api_secret_name}')
-    # raise e
-    # except userdata.NotebookAccessError as e:
-    # print(f'You need to grant this notebook access to the {gemini_api_secret_name} secret in order for the notebook to access Gemini on your behalf.')
-    # raise e
-    # except Exception as e:
-    # # unknown error
-    # print(f"There was an unknown error. Ensure you have a secret {gemini_api_secret_name} stored in Colab and it's a valid key from https://makersuite.google.com/app/apikey")
-    # raise e
+    item_selling = request.json['search']
+    discount = request.json['discount']
+    theme = request.json['theme']
+    count = 5
 
-    # model = genai.GenerativeModel('gemini-pro')
+    prompt_text = f"Generate creative images banner of {item_selling} restricted to {count} images. Make it appealing so that people will be interested to buy it. Choose background colour palette to maintain brand consistency"
+    if discount!=0:
+        prompt_text = prompt_text + f" Show image along with discount of {discount}%"
+    
+    if theme:
+        prompt_text = prompt_text + f" and theme of{theme}"
+    
+
+    print(prompt_text)
+
+    model = ImageGenerationModel.from_pretrained("imagegeneration@005")
+    images = model.generate_images(prompt=prompt_text)
+
+
+    num = random.random()
+    image_path = []
+    for image in images:
+        image.save(location=f"./img-{num}.png", include_generation_parameters=True)
+        image_path.append(f'./img-{num}.png')
+
+
+    # data = {prompt:response.text}
+    return prompt_text
