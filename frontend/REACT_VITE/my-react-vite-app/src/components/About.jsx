617ddbcafb2d367a75fe1136d78428ce3379054a
Pratham Choudhari <choudharipratham9@gmail.com>
2024-09-10 20:28:43 +0530

    pratham

---
+++ frontend/REACT_VITE/my-react-vite-app/src/components/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='w-full p-20 bg-[#9ef01a] rounded-3xl text-black'>
+      <h1 className='font-["Neue_Montreal"] text-[4vw] leading-[4.5vw] tracking-tight'>snapgen is a generate beautiful and high quality banners adaptable to various themes and promotional offers with imagen powered by vertex Ai studio. edit your images according to your requirements</h1>
+      <div className='w-full border-t-[1px] mt-20 border-[#a1b562]'></div>
+    </div>
+    
+  )
+}
+
+export default About 
---
+++ frontend/REACT_VITE/my-react-vite-app/src/components/Marquee.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Marquee() {
+  return (
+    <div className='w-full py-10 rounded-3xl bg-[#004b23]'>
+         <div className='text border-t-2 border-b-2 border-zin-300 flex gap-10 overflow-hidden whitespace-nowrap' >
+            <h1 className='text-[15vw] leading-none font["founders_Grotesk"] uppercase font-semibold pt-10 mb-[3vw]'>we are snapgen</h1>
+            <h1 className='text-[15vw] leading-none font["founders_Grotesk"] uppercase font-semibold pt-10 mb-[3vw]'>we are snapgen</h1>
+
+            
+         </div>
+    </div>
+  )
+}
+
+export default Marquee
