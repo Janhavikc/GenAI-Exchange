af55a557c59c2bced7b40ec66e6fba76729c69ab
Janhavi Choudhari <janhavi.choudhari@oracle.com>
2024-09-11 23:01:40 +0530

    Finished Image API

--- backend/routes.py
+++ backend/routes.py
@@ -5,12 +5,16 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import random
+from flask import send_from_directory
+
 
 # Load environment variables from .env file
 load_dotenv()
 
+upload = './'
 # Create a blueprint for routes
 main_routes = Blueprint('main', __name__)
+
 
 @main_routes.route("/imagen", methods=["POST"])
 def imagen():
@@ -30,20 +34,27 @@ def imagen():
         print(f"There was an unknown error. Ensure you have a secret {gemini_api_secret_name} stored and it's a valid key from https://makersuite.google.com/app/apikey")
         raise e
 
-    item_selling = request.json['search']
-    discount = request.json['discount']
-    theme = request.json['theme']
+    
+    obj = request.get_json()
+    item_selling = obj['search']
+    # discount = request.json['discount']
+    # theme = request.json['theme']
     count = 5
 
-    prompt_text = f"Generate creative images banner of {item_selling} restricted to {count} images. Make it appealing so that people will be interested to buy it. Choose background colour palette to maintain brand consistency"
-    if discount!=0:
-        prompt_text = prompt_text + f" Show image along with discount of {discount}%"
-    
-    if theme:
-        prompt_text = prompt_text + f" and theme of{theme}"
-    
+    # model = genai.GenerativeModel('gemini-pro')
+    # chat = model.start_chat(history=[])
 
-    print(prompt_text)
+    prompt_text = f"Generate creative a images of {item_selling}. Make it attractive so that more Indian people can buy it. Select colors to maintain brand consistency"
+    if 'discount' in obj:
+        discount = obj['discount']
+        prompt_text = prompt_text + f" in the image also show discount of {discount}%"
+    
+    if 'theme' in obj:
+        theme = obj['theme']
+        prompt_text = prompt_text + f" and set image theme as {theme}"
+    
+    # response = chat.send_message(prompt_text)
+    # print(response.text)
 
     model = ImageGenerationModel.from_pretrained("imagegeneration@005")
     images = model.generate_images(prompt=prompt_text)
@@ -53,8 +64,10 @@ def imagen():
     image_path = []
     for image in images:
         image.save(location=f"./img-{num}.png", include_generation_parameters=True)
-        image_path.append(f'./img-{num}.png')
+        image_path.append(f'img-{num}.png')
 
 
     # data = {prompt:response.text}
-    return prompt_text
+    return send_from_directory(
+        ".", image_path[0], as_attachment=True
+    )
