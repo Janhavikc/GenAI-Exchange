2d4db0583fdf0b514ee2a12263ae4314a3ba080c
Pratham Choudhari <choudharipratham9@gmail.com>
2024-09-13 19:34:10 +0530

    added logo

--- frontend/REACT_VITE/my-react-vite-app/src/components/LandingPage.jsx
+++ frontend/REACT_VITE/my-react-vite-app/src/components/LandingPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-
 import {FaArrowUpLong} from "react-icons/fa6";
+import { motion } from "framer-motion";
+
 function LandingPage() {
+    motion
   return (
-    <div className='w-full h-screen bg-zinc-900 pt-1'>
+    <div data-scroll data-scroll-section data-scroll-speed="-.3" className='w-full h-screen bg-zinc-900 pt-1'>
       <div className='textstructure mt-60 px-20'>
         {["Generate high quality","image banners and","video trailers with ai"].map((item,index)=>{
-          return <div className='masker' >
+          return <div key={index} className='masker' >
             <div className='w-fit flex item-center'>
-              {index === 1 && (<div className='w-[7vw] h-[5vw] -top-[2.2vw] rounded-md bg-[#004b23] mr-3'></div>)}
+              {index === 1 && (
+                <motion.div initial={{width:0}} animate={{width:"7vw"}} transition={{ease: [0.76, 0, 0.24, 1], duration:1}} className='w-[7vw] h-[5vw] -top-[2.2vw] rounded-md bg-[#004b23] mr-3'></motion.div>)}
           <h1 className="uppercase text-7xl leading-[5vw] tracking-tighter font-['founders_Grotesk'] font-medium">
             {item}
           </h1>
@@ -24,6 +27,7 @@ function LandingPage() {
         <div className=' px-5 py-2 w-10 h-10 flex items-center justify-center border-[1px] border-zin-300 rounded-full'>
           <span className='rotate-[45deg]'>
           <FaArrowUpLong/>
+
         </span>
         
         </div>
