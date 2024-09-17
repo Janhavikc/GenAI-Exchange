cea137982ffb777e9262badf37b9d00d4ac4fc6a
Pratham Choudhari <choudharipratham9@gmail.com>
2024-09-17 23:14:37 +0530

    uploaded robot

--- frontend/my-react-vite-app/src/components/LandingPage.jsx
+++ frontend/my-react-vite-app/src/components/LandingPage.jsx
@@ -10,10 +10,15 @@ function LandingPage() {
     navigate('/design');
   }
   return (
-    <div data-scroll data-scroll-section data-scroll-speed="-.3" className='w-full h-screen bg-zinc-900 pt-1'>
-      <div className='textstructure mt-60 px-20'>
+
+             <div  data-scroll data-scroll-section data-scroll-speed="-.3" className='w-full h-screen bg-zinc-900 pt-1'>
+            <div className='textstructure mt-60 px-20'>
+      
         {["Generate high quality","image banners and","video trailers with ai"].map((item,index)=>{
+          
           return <div key={index} className='masker' >
+            
+                 
             <div className='w-fit flex item-center'>
               {index === 1 && (
                 <motion.div initial={{width:0}} animate={{width:"7vw"}} transition={{ease: [0.76, 0, 0.24, 1], duration:1}} className='w-[7vw] h-[5vw] -top-[2.2vw] rounded-md bg-[#004b23] mr-3'></motion.div>)}
@@ -21,9 +26,12 @@ function LandingPage() {
             {item}
           </h1>
         </div>
-        </div>
-          })}
+             </div>
+             
+             
         
+              })}
+      
         
         <div className="border-t-[1px] border-zinc-800 mt-20 flex justify-between item-center py-5 px-20">
           {["",""].map((item,index)=>(<p className='text-md font-light tracking-tight leading-none'>{item}</p>))}
@@ -40,6 +48,9 @@ function LandingPage() {
         </div>
       </div>
     </div>
+    
+     
+
   )
 }
 
